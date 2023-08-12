f6251d1c377ba81db63ade90420a2cddce7db514
root <akosabenedict@gmail.com>
2023-08-12 06:11:08 -0700

    Hexa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,8 +14,8 @@ int main(void)
 	i = 0;
 	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
-	for (j = 0 ; j < 15 ; j++)
-		putchar(j + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
