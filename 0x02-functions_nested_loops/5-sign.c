67cb7fc27377cbdc1510df2d99c19178e8765738
root <akosabenedict@gmail.com>
2023-08-15 22:31:30 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,26 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_sign - check the code
  * @n: character to check
  * Return: 1 if n greater than zero and 0 or -1 for other character
  *
  */
 int print_sign(int n)
 {
-	n = 110
-
-	if (n > 0 && )
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
