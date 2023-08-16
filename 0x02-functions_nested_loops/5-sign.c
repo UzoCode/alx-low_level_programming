d028e176c1cef69d20622e534c749e3e354724d5
root <akosabenedict@gmail.com>
2023-08-15 21:25:59 -0700

    alpha

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @n: character to check
+ * Return: 1 if n greater than zero and 0 or -1 for other character
+ *
+ */
+int print_sign(int n)
+{
+	n = 110
+
+	if (n > 0 && )
+}
