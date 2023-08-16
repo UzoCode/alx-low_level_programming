b236c9a53b85b49a72e02f7d496c2b09d7c3c0fe
root <akosabenedict@gmail.com>
2023-08-16 10:07:34 -0700

    Ordered Num

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints the number in order
+ * @n: checks for ordered numbers
+ * Result: 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+}
