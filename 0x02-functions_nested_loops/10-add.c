adfb332597d6d8e80ea8b5624b09089dedcf55e1
root <akosabenedict@gmail.com>
2023-08-16 09:39:46 -0700

    Sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,13 @@
 
 /**
  * add - Adds two integer numbers
- *@int: Is checked
+ * @i: Is a variable
+ * @j: Another variable
  * Return: 0
  */
 int add(int i, int j)
 {
 	int num = i + j;
 
-	return num;
+	return (num);
 }
