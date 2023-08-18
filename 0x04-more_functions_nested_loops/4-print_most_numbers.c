172b91c1ec4c138cf337bebba5ab5c8e65d08392
root <akosabenedict@gmail.com>
2023-08-17 21:18:44 -0700

    Number and Signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,10 @@ void print_most_numbers(void)
 {
 	int a;
 
-	for (a = 48; a < 58; a++)
+	for (a = 48; a < 57; a++)
 	{
-		if (a == 2 || a == 4)
-			continue;
-		_putchar(a);
+		if (!(a == 50 || a == 52))
+			_putchar(a);
 	}
 	_putchar('\n');
 }
