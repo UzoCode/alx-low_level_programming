3d76b1e637877661743758046a1a5c3537381d8b
root <akosabenedict@gmail.com>
2023-08-17 20:18:36 -0700

    Number and signs

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number - print from 0 to 9
+ * Return: the printed numbers
+ */
+void print_numbers(void)
+{
+	int ai;
+
+	for (ai = 48; ai < 58; ai++)
+	{
+		_putchar(ai);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *
+ *
+ * Return: 0
+ *
+ */
+void print_most_numbers(void)
+{
+
+}
