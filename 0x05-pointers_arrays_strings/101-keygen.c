93b92d8388d1d9e1bbfd20d6836e32c87229e6eb
root <akosabenedict@gmail.com>
2023-08-22 08:18:20 -0700

    Key

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,18 +12,15 @@ int main(void)
 	int i, sum, n;
 
 	sum = 0;
-
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
-
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
-
 			sum += n;
 			putchar(n + '0');
 			break;
