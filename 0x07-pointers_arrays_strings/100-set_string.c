52cc8286c7ca6640c656a7a6109cab5ddd332cf1
root <akosabenedict@gmail.com>
2023-08-28 21:59:14 -0700

    String setting

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a cha
+ * @s: source address
+ * @to: target adress
+ * Return: None
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
