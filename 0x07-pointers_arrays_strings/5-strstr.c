164fe4acb2d8e747a1edade6676b043017fd5ba9
root <akosabenedict@gmail.com>
2023-08-28 20:57:21 -0700

    Substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr -  locates a substring
+ * @haystack: entire string
+ * @needle: substring
+ * Return: pointer to the beginniing of located substring
+ * or NULL if the substring is found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *ahaystack;
+	char *bneedle;
+
+	while (*haystack != '\0')
+	{
+		ahaystack = haystack;
+		bneedle = needle;
+
+		while (*haystack != '\0' && *bneedle != '\0' && *haystack == *bneedle)
+		{
+			haystack++;
+			bneedle++;
+		}
+		if (!*bneedle)
+			return (ahaystack);
+		haystack = ahaystack + 1;
+	}
+	return (0);
+}
