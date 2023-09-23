091277739226292b05d7a7c882ac0a51bff74e0a
root <akosabenedict@gmail.com>
2023-09-22 19:26:48 -0700

    Prior function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * prior_main - function executes before main
+ * Return: void
+ */
+void prior_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
