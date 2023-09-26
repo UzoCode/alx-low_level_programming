1aa819be5b8705a18fbe5e2acc449744f87e2aec
root <akosabenedict@gmail.com>
2023-09-26 02:42:31 -0700

    Print all Elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t Nnodes = 0;
+	size_t bnodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		Nnodes++;
+		bnodes++;
 	}
-	return (Nnodes);
+	return (bnodes);
 }
