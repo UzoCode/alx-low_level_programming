376fdf1e769c99bd40332fedd51be48f29d3a79f
Benedict Akosa <akosabenedict@gmail.com>
2023-12-01 20:25:21 -0800

    Shell Project

---
+++ Simple_Shell/README.md
@@ -0,0 +1,1 @@
+Basic of Simple Shell
---
+++ Simple_Shell/shell.h
@@ -0,0 +1,10 @@
+#ifndef _SHELL_H
+#define _SHELL_H
+
+#include <sys/type.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <>
+
+#endif
