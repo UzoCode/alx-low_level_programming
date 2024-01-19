f939ea3630f27624ae162f8852d941a28ae16931
Benedict Akosa <akosabenedict@gmail.com>
2024-01-19 08:02:00 -0800

    Script

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Compile all .c files into object files
-gcc -c -fPIC *.c
-
-# Create the dynamic library
-gcc -shared -o liball.so *.o
-
-# Clean up the temporary object files
-rm *.o
+gcc -c *.c -fpic && gcc *o -shared -o liball.so
